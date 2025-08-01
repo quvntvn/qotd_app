4d380f330c33dd2accba1870771aea8e10c8ba3f
quvntvn <hulkufe4@gmail.com>
2025-08-01 15:30:14 +0200

    cc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.quvntvn.qotd_app"
         minSdk = 28
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.1.7"
+        versionCode = 10
+        versionName = "1.1.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
